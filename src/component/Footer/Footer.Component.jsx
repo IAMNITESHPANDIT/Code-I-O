851f135f25da3a4683243e90f20e141b78da8d34
Admin <admin@Admins-iMac-2.local>
2022-05-24 06:04:24 -0700

    images added and also improved  home page and footer Page

--- src/component/Footer/Footer.Component.jsx
+++ src/component/Footer/Footer.Component.jsx
@@ -4,8 +4,6 @@ function FooterComponent() {
   return (
     <div>
       <footer class="container-fluid page-footer font-small text-color-dark pt-4 position-absolute bottom-0">
-        
-
         <div class="footer-copyright bg-black text-white text-center py-3 container-fluid">
           © 2022 Copyright:
           <a className="text-decoration-none text-white" href="/"> @Nitesh Pandey</a>
--- src/pages/Home/Home.Page.jsx
+++ src/pages/Home/Home.Page.jsx
@@ -5,16 +5,16 @@ function HomePage() {
   return (
       <div className='container'>
           <div className="row">
-              <div className='col-12 pt-5 text-center col-md-6'>
-                  <h2 className='h2'>Welcome to <strong>Code </strong><span className='text-success'>I/O</span></h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptatum delectus quibusdam aut iusto. Iure ad commodi laboriosam eius fugiat quo nemo. Fugiat quaerat ipsam accusantium dolorem deleniti praesentium voluptas quae enim repudiandae, quam quidem tenetur maxime, eos sit! Enim.
+              <div className='col-12 pt-5 text-center col-md-6 '>
+                  <h2 className='h2 text-start'>Welcome to <strong>Code </strong><span className='text-success'>I/O</span></h2>
+                    <p className='text-start'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptatum delectus quibusdam aut iusto. Iure ad commodi laboriosam eius fugiat quo nemo. Fugiat quaerat ipsam accusantium dolorem deleniti praesentium voluptas quae enim repudiandae, quam quidem tenetur maxime, eos sit! Enim.
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto sit optio harum quos, dignissimos perferendis vitae itaque obcaecati beatae fugiat, odit iste deserunt ducimus. Alias similique enim soluta explicabo! Corporis voluptates incidunt ad magnam, nam voluptas, neque asperiores accusamus numquam totam, maxime alias voluptatem ex optio provident error labore? Mollitia.
                     </p>
                     <button className='btn btn-outline-primary me-4 p-3'>Register Now!</button>
                     <button className='btn btn-outline-success p-2'>Login Now!</button>
               </div>
-              <div className='col-12 col-md-6 p-4 mb-5 d-none d-md-block'>
-              <img className="img-fluid" alt="Coding logo" src={Diamond}/>
+              <div className='col-12 col-md-6 p-4 mb-5 d-none d-md-block mt-5'>
+              <img className="img-fluid " alt="Coding logo" src={Diamond}/>
               </div>
 
           </div>
