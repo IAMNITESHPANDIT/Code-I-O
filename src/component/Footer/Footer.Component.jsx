af646e4241d37ff7e191bbcc0a17735a485d71fa
nitesh <niteshpandit676@gmail.com>
2022-05-24 22:04:58 +0530

    footer style converted into img-fluid

--- src/component/Footer/Footer.Component.jsx
+++ src/component/Footer/Footer.Component.jsx
@@ -3,12 +3,11 @@ import React from "react";
 function FooterComponent() {
   return (
     <div>
-      <footer class="container-fluid  font-small text-color-dark pt-4 fixed-bottom">
-        <div class="footer-copyright bg-black text-white text-center p-3 ">
+        <div class="footer-copyright bg-black text-white text-center p-3 fixed-bottom">
           © 2022 Copyright:
           <a className="text-decoration-none text-white" href="/"> @Nitesh Pandey</a>
         </div>
-      </footer>
+      
     </div>
   );
 }
