4c4fccb86fc22295a4c026945822690b9b6e5a6c
Admin <admin@Admins-iMac-2.local>
2022-05-24 05:40:09 -0700

    title updated and added  welecome text in  website

--- src/pages/Home/Home.Page.jsx
+++ src/pages/Home/Home.Page.jsx
@@ -6,6 +6,7 @@ function HomePage() {
       <div className='container'>
           <div className="row">
               <div className='col-12 pt-5 text-center col-md-6'>
+                  <h2 className='h2'>Welcome to <strong>Code </strong><span className='text-success'>I/O</span></h2>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptatum delectus quibusdam aut iusto. Iure ad commodi laboriosam eius fugiat quo nemo. Fugiat quaerat ipsam accusantium dolorem deleniti praesentium voluptas quae enim repudiandae, quam quidem tenetur maxime, eos sit! Enim.
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto sit optio harum quos, dignissimos perferendis vitae itaque obcaecati beatae fugiat, odit iste deserunt ducimus. Alias similique enim soluta explicabo! Corporis voluptates incidunt ad magnam, nam voluptas, neque asperiores accusamus numquam totam, maxime alias voluptatem ex optio provident error labore? Mollitia.
                     </p>
