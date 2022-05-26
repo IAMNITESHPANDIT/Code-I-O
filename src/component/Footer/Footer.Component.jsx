2490833baf79ec52a8279b2b474063796d3eb35f
Admin <admin@Admins-iMac-2.local>
2022-05-26 14:17:37 +0530

    bug fixed in home and footer component

--- src/component/Footer/Footer.Component.jsx
+++ src/component/Footer/Footer.Component.jsx
@@ -4,43 +4,43 @@ function FooterComponent() {
   return (
     <div>
       <div className="footer-copyright bg-black text-white text-center p-3 fixed-bottom row">
-        <div className="col-4">
+        <div className="col-4 col-md-3">
           <a
             className="text-decoration-none text-white"
             href="https://github.com/iamniteshpandit"
           >
-            <i class="fa-brands fa-github ms-2"></i>
+            <i className="fa-brands fa-github ms-2"></i>
           </a>
 
           <a
             className="text-decoration-none text-white"
             href="https://in.linkedin.com/in/iamniteshpandit?original_referer=https%3A%2F%2Fwww.google.com%2F"
           >
-            <i class="fa-brands fa-linkedin ms-2"></i>
+            <i className="fa-brands fa-linkedin ms-2"></i>
           </a>
 
           <a
             className="text-decoration-none text-white"
             href="https://facebook.com/iamniteshpandit"
           >
-            <i class="fa-brands fa-facebook ms-2"></i>
+            <i className="fa-brands fa-facebook ms-2"></i>
           </a>
 
           <a
             className="text-decoration-none text-white"
             href="https://instagram.com/iamniteshpandit"
           >
-            <i class="fa-brands fa-instagram ms-2"></i>
+            <i className="fa-brands fa-instagram ms-2"></i>
           </a>
 
           <a
             className="text-decoration-none text-white"
             href="https://twitter.com/iamniteshpandit"
           >
-            <i class="fa-brands fa-twitter ms-2"></i>
+            <i className="fa-brands fa-twitter ms-2"></i>
           </a>
         </div>
-        <div className="col-8">
+        <div className="col-8 col-md-9">
           © 2022 Copyright:
           <a className="text-decoration-none text-white" href="/">
             {" "}
